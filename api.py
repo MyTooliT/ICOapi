cefddb1f1543a158deee5772ba851c649fba335b
Moritz Breurather <breurather@ift.at>
2024-07-16 12:42:30 +0200

    Backend: Add simple delay route for testing, add cors

--- api.py
+++ api.py
@@ -1,13 +1,36 @@
+import time
+
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+
 from config import Settings
 
 settings = Settings()
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/ping", status_code=status.HTTP_200_OK)
 def ping():
     return "OK"
+
+
+@app.get("/delay", status_code=status.HTTP_200_OK)
+def delay():
+    time.sleep(1)
+    return "OK"
+
 
 if __name__ == "__main__":
     import asyncio
