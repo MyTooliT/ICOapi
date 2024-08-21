43b49bc654cd1d415dd113dd20d6aac6f40cea1f
Moritz Breurather <breurather@ift.at>
2024-08-21 07:10:31 +0200

    Backend: Send empty list if no STH is found

--- routers/sth_routes.py
+++ routers/sth_routes.py
@@ -30,7 +30,7 @@ async def sth(response: Response) -> list[STHDeviceResponseModel]:
     devices = await get_sth_devices_from_network()
     if len(devices) == 0:
         response.status_code = status.HTTP_204_NO_CONTENT
-
+        return list()
     return [STHDeviceResponseModel.from_network(device) for device in devices]
 
 
