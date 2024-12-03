b6806dc0c0411a287447d410b1c1142d2da0fd94
Moritz Breurather <breurather@ift.at>
2024-12-03 10:57:30 +0100

    Backend: Prevent directory traversal

--- routers/file_routes.py
+++ routers/file_routes.py
@@ -2,47 +2,11 @@ from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
 from fastapi.responses import FileResponse
 import os
-from dotenv import load_dotenv
 from datetime import datetime
 from models.models import MeasurementFileDetails
+from scripts.file_handling import get_measurement_dir, tries_to_traverse_directory
 
 router = APIRouter(prefix="/files")
-
-def get_measurement_dir() -> str:
-    """To be used for dependency injection."""
-    measurement_dir = "icogui"
-    env_loaded = load_dotenv("../ico-front/.env")
-    if env_loaded:
-        measurement_dir = os.getenv("VITE_BACKEND_MEASUREMENT_DIR")
-    data_dir = ""
-
-    if os.name == "nt":
-        print("Found WINDOWS system.")
-        data_dir = os.getenv("LOCALAPPDATA")
-    elif os.name == "posix":
-        print("Found POSIX system.")
-        data_dir = linux_get_preferred_data_dir(measurement_dir)
-
-    return os.path.join(data_dir, measurement_dir)
-
-
-def linux_get_xdg_data_dirs() -> list[str]:
-    """Get data directories for LINUX systems"""
-    # Get XDG_DATA_DIRS or use the default value
-    xdg_data_dirs = os.getenv("XDG_DATA_DIRS", "/usr/local/share:/usr/share")
-    # Split the colon-separated paths into a list
-    return xdg_data_dirs.split(":")
-
-
-def linux_get_preferred_data_dir(app_name: str) -> str:
-    """Get usable data directory for LINUX systems"""
-    # Iterate through XDG_DATA_DIRS and pick the first writable directory
-    for data_dir in linux_get_xdg_data_dirs():
-        app_data_dir = os.path.join(data_dir, app_name)
-        if os.access(data_dir, os.W_OK):  # Check if the directory is writable
-            os.makedirs(app_data_dir, exist_ok=True)
-            return app_data_dir
-    raise PermissionError("No writable XDG_DATA_DIRS found")
 
 
 @router.get("")
@@ -71,7 +35,13 @@ async def list_files(measurement_dir: str = Depends(get_measurement_dir)) -> lis
 
 @router.get("/{name}")
 async def download_file(name: str, measurement_dir: str = Depends(get_measurement_dir)):
+
+    # Prevent directory traversal
+    if tries_to_traverse_directory(name):
+        raise HTTPException(status_code=405, detail="Method not allowed")
+
     full_path = os.path.join(measurement_dir, name)
+    print(full_path)
     if os.path.isfile(full_path):
         return FileResponse(path=full_path, filename=name)
     else:
@@ -79,6 +49,11 @@ async def download_file(name: str, measurement_dir: str = Depends(get_measuremen
 
 @router.delete("/{name}")
 async def delete_file(name: str, measurement_dir: str = Depends(get_measurement_dir)):
+
+    # Prevent directory traversal
+    if tries_to_traverse_directory(name):
+        raise HTTPException(status_code=405, detail="Method not allowed")
+
     full_path = os.path.join(measurement_dir, name)
     if os.path.isfile(full_path):
         try:
