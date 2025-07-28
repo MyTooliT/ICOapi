5489629179aeae29c7ef3630427d01847eb28261
René Schwaiger <sanssecours@me.com>
2025-07-28 15:49:56 +0200

    Test: Extend test for STU root endpoint

--- test/test_stu.py
+++ test/test_stu.py
@@ -1,6 +1,7 @@
 # -- Imports ------------------------------------------------------------------
 
 from fastapi.testclient import TestClient
+from netaddr import EUI
 
 from icoapi.api import app
 
@@ -17,3 +18,10 @@ def test_root() -> None:
 
     response = client.get(stu_prefix)
     assert response.status_code == 200
+    sth_response = response.json()[0]
+    assert sth_response["device_number"] == 1
+    mac_address = sth_response["mac_address"]
+    # Check that MAC address is valid and assert that it is equal to (the
+    # string representation) of itself
+    assert EUI(mac_address) == mac_address
+    assert sth_response["name"] == "STU 1"
