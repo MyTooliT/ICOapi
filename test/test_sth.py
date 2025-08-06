f7eee1f32ecc6ca1ad9f02a16720db832ede22f9
René Schwaiger <sanssecours@me.com>
2025-08-06 15:09:21 +0200

    Test: Collect STH tests in class

--- test/test_sth.py
+++ test/test_sth.py
@@ -43,110 +43,108 @@ async def get_and_connect_test_sensor_node(client) -> dict[str, Any]:
 # -- Tests --------------------------------------------------------------------
 
 
-@mark.anyio
-async def test_root(client) -> None:
-    """Test endpoint ``/``"""
+@mark.usefixtures("anyio_backend")
+class TestSTH:
 
-    response = await client.get(sth_prefix)
+    async def test_root(self, client) -> None:
+        """Test endpoint ``/``"""
 
-    assert response.status_code == 200
-    sensor_devices = response.json()
-    # We assume that at least one sensor device is available
-    assert len(sensor_devices) >= 1
+        response = await client.get(sth_prefix)
 
-    sensor_device = sensor_devices[0]
-    assert sensor_device["device_number"] == 0
-    mac_address = sensor_device["mac_address"]
-    # Check that MAC address is valid and assert that it is equal to (the
-    # string representation) of itself
-    assert EUI(mac_address) == mac_address
-    assert len(sensor_device["name"]) <= 8
-    assert 0 >= sensor_device["rssi"] >= -80
+        assert response.status_code == 200
+        sensor_devices = response.json()
+        # We assume that at least one sensor device is available
+        assert len(sensor_devices) >= 1
 
+        sensor_device = sensor_devices[0]
+        assert sensor_device["device_number"] == 0
+        mac_address = sensor_device["mac_address"]
+        # Check that MAC address is valid and assert that it is equal to (the
+        # string representation) of itself
+        assert EUI(mac_address) == mac_address
+        assert len(sensor_device["name"]) <= 8
+        assert 0 >= sensor_device["rssi"] >= -80
 
-@mark.anyio
-async def test_connect_disconnect(client) -> None:
-    """Test endpoint ``/connect`` and ``disconnect``"""
+    async def test_connect_disconnect(self, client) -> None:
+        """Test endpoint ``/connect`` and ``disconnect``"""
 
-    # ========================
-    # = Test Normal Response =
-    # ========================
+        # ========================
+        # = Test Normal Response =
+        # ========================
 
-    await get_and_connect_test_sensor_node(client)
+        await get_and_connect_test_sensor_node(client)
 
-    await client.put(f"{sth_prefix}/disconnect")
+        await client.put(f"{sth_prefix}/disconnect")
 
-    # =======================
-    # = Test Error Response =
-    # =======================
+        # =======================
+        # = Test Error Response =
+        # =======================
 
-    response = await client.put(
-        f"{sth_prefix}/connect", json={"mac": "01-02-03-04-05-06"}
-    )
+        response = await client.put(
+            f"{sth_prefix}/connect", json={"mac": "01-02-03-04-05-06"}
+        )
 
-    assert response.status_code == 404
+        assert response.status_code == 404
 
+    async def test_rename(self, client) -> None:
+        """Test endpoint ``/rename``"""
 
-@mark.anyio
-async def test_rename(client) -> None:
-    """Test endpoint ``/rename``"""
+        sensor_node = await get_and_connect_test_sensor_node(client)
+        mac_address = sensor_node["mac_address"]
 
-    sensor_node = await get_and_connect_test_sensor_node(client)
-    mac_address = sensor_node["mac_address"]
+        response = await client.put(
+            f"{sth_prefix}/rename",
+            json={"mac_address": mac_address, "new_name": "Hello"},
+        )
+        assert response.status_code == 200
+        assert isinstance(response.json(), dict)
+        old_name = response.json()["old_name"]
+        name = response.json()["name"]
+        assert name == "Hello"
 
-    response = await client.put(
-        f"{sth_prefix}/rename",
-        json={"mac_address": mac_address, "new_name": "Hello"},
-    )
-    assert response.status_code == 200
-    assert isinstance(response.json(), dict)
-    old_name = response.json()["old_name"]
-    name = response.json()["name"]
-    assert name == "Hello"
+        response = await client.put(
+            f"{sth_prefix}/rename",
+            json={"mac_address": mac_address, "new_name": old_name},
+        )
+        assert isinstance(response.json(), dict)
+        assert response.json()["old_name"] == name
+        assert response.json()["name"] == old_name
 
-    response = await client.put(
-        f"{sth_prefix}/rename",
-        json={"mac_address": mac_address, "new_name": old_name},
-    )
-    assert isinstance(response.json(), dict)
-    assert response.json()["old_name"] == name
-    assert response.json()["name"] == old_name
+        await client.put(f"{sth_prefix}/disconnect")
 
-    await client.put(f"{sth_prefix}/disconnect")
+    async def test_read_adc(self, client) -> None:
+        """Test endpoint ``/read-adc``"""
 
+        await get_and_connect_test_sensor_node(client)
 
-@mark.anyio
-async def test_read_adc(client) -> None:
-    """Test endpoint ``/read-adc``"""
+        response = await client.get(f"{sth_prefix}/read-adc")
+        assert response.status_code == 200
+        adc_configuration = response.json()
+        adc_attributes_int = {
+            "prescaler",
+            "acquisition_time",
+            "oversampling_rate",
+        }
+        for attribute in adc_attributes_int:
+            assert attribute in adc_configuration
+            assert isinstance(adc_configuration[attribute], int)
+        assert "reference_voltage" in adc_configuration
+        assert isinstance(adc_configuration["reference_voltage"], float)
 
-    await get_and_connect_test_sensor_node(client)
+        await client.put(f"{sth_prefix}/disconnect")
 
-    response = await client.get(f"{sth_prefix}/read-adc")
-    assert response.status_code == 200
-    adc_configuration = response.json()
-    adc_attributes_int = {"prescaler", "acquisition_time", "oversampling_rate"}
-    for attribute in adc_attributes_int:
-        assert attribute in adc_configuration
-        assert isinstance(adc_configuration[attribute], int)
-    assert "reference_voltage" in adc_configuration
-    assert isinstance(adc_configuration["reference_voltage"], float)
+    async def test_write_adc(self, client) -> None:
+        """Test endpoint ``/write-adc``"""
 
-    await client.put(f"{sth_prefix}/disconnect")
+        await get_and_connect_test_sensor_node(client)
 
+        response = await client.get(f"{sth_prefix}/read-adc")
+        assert response.status_code == 200
+        adc_configuration = response.json()
 
-@mark.anyio
-async def test_write_adc(client) -> None:
-    """Test endpoint ``/write-adc``"""
+        response = await client.put(
+            f"{sth_prefix}/write-adc",
+            json=adc_configuration,
+        )
 
-    await get_and_connect_test_sensor_node(client)
-
-    response = await client.get(f"{sth_prefix}/read-adc")
-    assert response.status_code == 200
-    adc_configuration = response.json()
-
-    response = await client.put(
-        f"{sth_prefix}/write-adc",
-        json=adc_configuration,
-    )
-
-    await client.put(f"{sth_prefix}/disconnect")
+        await client.put(f"{sth_prefix}/disconnect")
