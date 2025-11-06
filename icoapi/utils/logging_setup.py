24120bbc2ca203415ae428b929ef79fafa7fbd77
René Schwaiger <sanssecours@me.com>
2025-11-06 14:51:58 +0100

    Lint: Fix mypy warning about undefined attribute

--- icoapi/utils/logging_setup.py
+++ icoapi/utils/logging_setup.py
@@ -21,7 +21,8 @@ log_queue: asyncio.Queue[str] = asyncio.Queue()
 
 load_env_file()
 
-LOG_LEVEL = os.getenv("LOG_LEVEL").upper()
+log_level = os.getenv("LOG_LEVEL")
+LOG_LEVEL = None if log_level is None else log_level.upper()
 LOG_USE_JSON = os.getenv("LOG_USE_JSON", "0") == "1"
 LOG_USE_COLOR = os.getenv("LOG_USE_COLOR", "0") == "1"
 LOG_MAX_BYTES = int(os.getenv("LOG_MAX_BYTES", 5 * 1024 * 1024))
