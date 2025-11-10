b7698139bba0243dc9ba74aef7d312b6eafc9486
René Schwaiger <sanssecours@me.com>
2025-11-10 14:31:53 +0100

    Sensor Test: Fix Pylint warnings

--- test/test_sensor.py
+++ test/test_sensor.py
@@ -1,43 +1,43 @@
+"""Tests for sensor endpoint"""
+
 # -- Imports ------------------------------------------------------------------
 
 from types import NoneType
 
-# -- Classes ------------------------------------------------------------------
+# -- Functions ----------------------------------------------------------------
 
 
-class TestSensor:
+def test_sensor(sensor_prefix, client) -> None:
+    """Test endpoint ``/sensor``"""
 
-    def test_sensor(self, sensor_prefix, client) -> None:
-        """Test endpoint ``/sensor``"""
+    response = client.get(sensor_prefix)
 
-        response = client.get(sensor_prefix)
+    assert response.status_code == 200
 
-        assert response.status_code == 200
+    body = response.json()
+    assert isinstance(body, dict)
 
-        body = response.json()
-        assert isinstance(body, dict)
+    sensors = body["sensors"]
+    assert isinstance(sensors, list)
 
-        sensors = body["sensors"]
-        assert isinstance(sensors, list)
+    for sensor in sensors:
+        for key in ("name", "sensor_id", "unit", "dimension"):
+            assert isinstance(sensor[key], str)
+        for key in (
+            "phys_min",
+            "phys_max",
+            "volt_min",
+            "volt_max",
+            "scaling_factor",
+            "offset",
+        ):
+            assert isinstance(sensor[key], float)
+        assert isinstance(sensor["sensor_type"], (str, NoneType))
 
-        for sensor in sensors:
-            for key in ("name", "sensor_id", "unit", "dimension"):
-                assert isinstance(sensor[key], str)
-            for key in (
-                "phys_min",
-                "phys_max",
-                "volt_min",
-                "volt_max",
-                "scaling_factor",
-                "offset",
-            ):
-                assert isinstance(sensor[key], float)
-            assert isinstance(sensor["sensor_type"], (str, NoneType))
+    configurations = body["configurations"]
+    assert isinstance(configurations, list)
 
-        configurations = body["configurations"]
-        assert isinstance(configurations, list)
-
-        for configuration in configurations:
-            for key in ("configuration_id", "configuration_name"):
-                assert isinstance(configuration[key], str)
-            assert isinstance(configuration["channels"], dict)
+    for configuration in configurations:
+        for key in ("configuration_id", "configuration_name"):
+            assert isinstance(configuration[key], str)
+        assert isinstance(configuration["channels"], dict)
