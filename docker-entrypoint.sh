a4af278971c451e1883bd3baad3cb9e795fef413
Moritz Breurather <78233097+wementoat@users.noreply.github.com>
2026-01-20 14:00:00 +0100

    Update docker-entrypoint.sh
    Implement shellcheck suggestion
    
    Co-authored-by: René Schwaiger <sanssecours@me.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Load the first *.env file found in icoapi/config
-ENV_FILE="$(ls icoapi/config/*.env 2>/dev/null | head -n 1)"
+ENV_FILE="$(find icoapi/config -name '*.env' 2>/dev/null | head -n 1)"
 
 if [ -n "$ENV_FILE" ]; then
   echo "Loading env from $ENV_FILE"
